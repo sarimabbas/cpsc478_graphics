45617553c6840cb2a556a0d79ae4a3967993e33b
Sarim Abbas <sarim.stays@gmail.com>
2020-02-08 13:45:05 -0500

    remove 2.4, add HOURS

--- hw2/q2.3/chapter3/primitives.js
+++ hw2/q2.3/chapter3/primitives.js
@@ -3,4 +3,4 @@ document.write('<script type="text/javascript" src="cone.js"></script>');
 document.write('<script type="text/javascript" src="cylinder.js"></script>');
 // prettier-ignore
 document.write('<script type="text/javascript" src="circularstreet.js"></script>');
-document.write('<script type="text/javascript" src="octahedron.js"></script>');
+document.write('<script type="text/javascript" src="sphere.js"></script>');
