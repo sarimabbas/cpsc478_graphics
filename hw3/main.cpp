8d76f2bc3c21ee104280605884d36db63af456fe
Sarim Abbas <sarim.stays@gmail.com>
2020-02-16 23:49:31 -0500

    prettier main

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,8 +1,8 @@
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
-#include <vector>
 #include <iostream>
+#include <vector>
 
 #include "SETTINGS.h"
 
@@ -10,209 +10,201 @@ using namespace std;
 
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
-VEC3 truncate(const VEC4& v)
-{
-  return VEC3(v[0], v[1], v[2]);
-}
-VEC4 extend(const VEC3& v)
-{
-  return VEC4(v[0], v[1], v[2], 1.0);
-}
+VEC3 truncate(const VEC4& v) { return VEC3(v[0], v[1], v[2]); }
+VEC4 extend(const VEC3& v) { return VEC4(v[0], v[1], v[2], 1.0); }
 
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
-void readPPM(const string& filename, int& xRes, int& yRes, float*& values)
-{
-  // try to open the file
-  FILE *fp;
-  fp = fopen(filename.c_str(), "rb");
-  if (fp == NULL)
-  {
-    cout << " Could not open file \"" << filename.c_str() << "\" for reading." << endl;
-    cout << " Make sure you're not trying to read from a weird location or with a " << endl;
-    cout << " strange filename. Bailing ... " << endl;
-    exit(0);
-  }
+void readPPM(const string& filename, int& xRes, int& yRes, float*& values) {
+    // try to open the file
+    FILE* fp;
+    fp = fopen(filename.c_str(), "rb");
+    if (fp == NULL) {
+        cout << " Could not open file \"" << filename.c_str()
+             << "\" for reading." << endl;
+        cout << " Make sure you're not trying to read from a weird location or "
+                "with a "
+             << endl;
+        cout << " strange filename. Bailing ... " << endl;
+        exit(0);
+    }
 
-  // get the dimensions
-  fscanf(fp, "P6\n%d %d\n255\n", &xRes, &yRes);
-  int totalCells = xRes * yRes;
+    // get the dimensions
+    fscanf(fp, "P6\n%d %d\n255\n", &xRes, &yRes);
+    int totalCells = xRes * yRes;
 
-  // grab the pixel values
-  unsigned char* pixels = new unsigned char[3 * totalCells];
-  fread(pixels, 1, totalCells * 3, fp);
+    // grab the pixel values
+    unsigned char* pixels = new unsigned char[3 * totalCells];
+    fread(pixels, 1, totalCells * 3, fp);
 
-  // copy to a nicer data type
-  values = new float[3 * totalCells];
-  for (int i = 0; i < 3 * totalCells; i++)
-    values[i] = pixels[i];
+    // copy to a nicer data type
+    values = new float[3 * totalCells];
+    for (int i = 0; i < 3 * totalCells; i++) values[i] = pixels[i];
 
-  // clean up
-  delete[] pixels;
-  fclose(fp);
-  cout << " Read in file " << filename.c_str() << endl;
+    // clean up
+    delete[] pixels;
+    fclose(fp);
+    cout << " Read in file " << filename.c_str() << endl;
 }
 
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
-void writePPM(const string& filename, int& xRes, int& yRes, const float* values)
-{
-  int totalCells = xRes * yRes;
-  unsigned char* pixels = new unsigned char[3 * totalCells];
-  for (int i = 0; i < 3 * totalCells; i++)
-    pixels[i] = values[i];
+void writePPM(const string& filename, int& xRes, int& yRes,
+              const float* values) {
+    int totalCells = xRes * yRes;
+    unsigned char* pixels = new unsigned char[3 * totalCells];
+    for (int i = 0; i < 3 * totalCells; i++) pixels[i] = values[i];
 
-  FILE *fp;
-  fp = fopen(filename.c_str(), "wb");
-  if (fp == NULL)
-  {
-    cout << " Could not open file \"" << filename.c_str() << "\" for writing." << endl;
-    cout << " Make sure you're not trying to write from a weird location or with a " << endl;
-    cout << " strange filename. Bailing ... " << endl;
-    exit(0);
-  }
+    FILE* fp;
+    fp = fopen(filename.c_str(), "wb");
+    if (fp == NULL) {
+        cout << " Could not open file \"" << filename.c_str()
+             << "\" for writing." << endl;
+        cout << " Make sure you're not trying to write from a weird location "
+                "or with a "
+             << endl;
+        cout << " strange filename. Bailing ... " << endl;
+        exit(0);
+    }
 
-  fprintf(fp, "P6\n%d %d\n255\n", xRes, yRes);
-  fwrite(pixels, 1, totalCells * 3, fp);
-  fclose(fp);
-  delete[] pixels;
+    fprintf(fp, "P6\n%d %d\n255\n", xRes, yRes);
+    fwrite(pixels, 1, totalCells * 3, fp);
+    fclose(fp);
+    delete[] pixels;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 // build out a single square
 //////////////////////////////////////////////////////////////////////////////////
-void buildBigSquare(vector<VEC3>& vertices, vector<VEC3I>& indices, vector<VEC3>& colors)
-{
-  vertices.push_back(VEC3(1.0, 1.0, 1.5));
-  vertices.push_back(VEC3( 11.0, 1.0, 1.5));
-  vertices.push_back(VEC3(1.0,  11.0, 1.5));
-  vertices.push_back(VEC3( 11.0,  11.0, 1.5));
+void buildBigSquare(vector<VEC3>& vertices, vector<VEC3I>& indices,
+                    vector<VEC3>& colors) {
+    vertices.push_back(VEC3(1.0, 1.0, 1.5));
+    vertices.push_back(VEC3(11.0, 1.0, 1.5));
+    vertices.push_back(VEC3(1.0, 11.0, 1.5));
+    vertices.push_back(VEC3(11.0, 11.0, 1.5));
 
-  // front face
-  indices.push_back(VEC3I(0, 1, 2));
-  indices.push_back(VEC3I(2, 1, 3));
-  colors.push_back(VEC3(1.0, 0.0, 0.0));
-  colors.push_back(VEC3(0.0, 0.0, 1.0));
+    // front face
+    indices.push_back(VEC3I(0, 1, 2));
+    indices.push_back(VEC3I(2, 1, 3));
+    colors.push_back(VEC3(1.0, 0.0, 0.0));
+    colors.push_back(VEC3(0.0, 0.0, 1.0));
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 // build out a single square
 //////////////////////////////////////////////////////////////////////////////////
-void buildSquare(vector<VEC3>& vertices, vector<VEC3I>& indices, vector<VEC3>& colors)
-{
-  vertices.push_back(VEC3(-0.5, -0.5,  0.5));
-  vertices.push_back(VEC3( 0.5, -0.5,  0.5));
-  vertices.push_back(VEC3(-0.5,  0.5,  0.5));
-  vertices.push_back(VEC3( 0.5,  0.5,  0.5));
+void buildSquare(vector<VEC3>& vertices, vector<VEC3I>& indices,
+                 vector<VEC3>& colors) {
+    vertices.push_back(VEC3(-0.5, -0.5, 0.5));
+    vertices.push_back(VEC3(0.5, -0.5, 0.5));
+    vertices.push_back(VEC3(-0.5, 0.5, 0.5));
+    vertices.push_back(VEC3(0.5, 0.5, 0.5));
 
-  // front face
-  indices.push_back(VEC3I(0, 1, 2));
-  indices.push_back(VEC3I(2, 1, 3));
-  colors.push_back(VEC3(1.0, 0.0, 0.0));
-  colors.push_back(VEC3(0.0, 1.0, 0.0));
+    // front face
+    indices.push_back(VEC3I(0, 1, 2));
+    indices.push_back(VEC3I(2, 1, 3));
+    colors.push_back(VEC3(1.0, 0.0, 0.0));
+    colors.push_back(VEC3(0.0, 1.0, 0.0));
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 // build out a cube
 //////////////////////////////////////////////////////////////////////////////////
-void buildCube(vector<VEC3>& vertices, vector<VEC3I>& indices, vector<VEC3>& colors)
-{
-  vertices.push_back(VEC3(-0.5, -0.5,  0.5));
-  vertices.push_back(VEC3( 0.5, -0.5,  0.5));
-  vertices.push_back(VEC3(-0.5,  0.5,  0.5));
-  vertices.push_back(VEC3( 0.5,  0.5,  0.5));
-  vertices.push_back(VEC3(-0.5, -0.5, -0.5));
-  vertices.push_back(VEC3( 0.5, -0.5, -0.5));
-  vertices.push_back(VEC3(-0.5,  0.5, -0.5));
-  vertices.push_back(VEC3( 0.5,  0.5, -0.5));
+void buildCube(vector<VEC3>& vertices, vector<VEC3I>& indices,
+               vector<VEC3>& colors) {
+    vertices.push_back(VEC3(-0.5, -0.5, 0.5));
+    vertices.push_back(VEC3(0.5, -0.5, 0.5));
+    vertices.push_back(VEC3(-0.5, 0.5, 0.5));
+    vertices.push_back(VEC3(0.5, 0.5, 0.5));
+    vertices.push_back(VEC3(-0.5, -0.5, -0.5));
+    vertices.push_back(VEC3(0.5, -0.5, -0.5));
+    vertices.push_back(VEC3(-0.5, 0.5, -0.5));
+    vertices.push_back(VEC3(0.5, 0.5, -0.5));
 
-  // front face
-  indices.push_back(VEC3I(0, 1, 2));
-  indices.push_back(VEC3I(2, 1, 3));
-  colors.push_back(VEC3(1.0, 0.0, 0.0));
-  colors.push_back(VEC3(1.0, 0.0, 0.0));
+    // front face
+    indices.push_back(VEC3I(0, 1, 2));
+    indices.push_back(VEC3I(2, 1, 3));
+    colors.push_back(VEC3(1.0, 0.0, 0.0));
+    colors.push_back(VEC3(1.0, 0.0, 0.0));
 
-  // back face
-  indices.push_back(VEC3I(5, 4, 7));
-  indices.push_back(VEC3I(7, 4, 6));
-  colors.push_back(VEC3(0.0, 1.0, 0.0));
-  colors.push_back(VEC3(0.0, 1.0, 0.0));
+    // back face
+    indices.push_back(VEC3I(5, 4, 7));
+    indices.push_back(VEC3I(7, 4, 6));
+    colors.push_back(VEC3(0.0, 1.0, 0.0));
+    colors.push_back(VEC3(0.0, 1.0, 0.0));
 
-  // left face
-  indices.push_back(VEC3I(4, 0, 6));
-  indices.push_back(VEC3I(6, 0, 2));
-  colors.push_back(VEC3(0.0, 0.0, 1.0));
-  colors.push_back(VEC3(0.0, 0.0, 1.0));
+    // left face
+    indices.push_back(VEC3I(4, 0, 6));
+    indices.push_back(VEC3I(6, 0, 2));
+    colors.push_back(VEC3(0.0, 0.0, 1.0));
+    colors.push_back(VEC3(0.0, 0.0, 1.0));
 
-  // right face
-  indices.push_back(VEC3I(1, 5, 3));
-  indices.push_back(VEC3I(3, 5, 7));
-  colors.push_back(VEC3(0.0, 1.0, 1.0));
-  colors.push_back(VEC3(0.0, 1.0, 1.0));
+    // right face
+    indices.push_back(VEC3I(1, 5, 3));
+    indices.push_back(VEC3I(3, 5, 7));
+    colors.push_back(VEC3(0.0, 1.0, 1.0));
+    colors.push_back(VEC3(0.0, 1.0, 1.0));
 
-  // top face
-  indices.push_back(VEC3I(2, 3, 6));
-  indices.push_back(VEC3I(6, 3, 7));
-  colors.push_back(VEC3(1.0, 1.0, 0.0));
-  colors.push_back(VEC3(1.0, 1.0, 0.0));
+    // top face
+    indices.push_back(VEC3I(2, 3, 6));
+    indices.push_back(VEC3I(6, 3, 7));
+    colors.push_back(VEC3(1.0, 1.0, 0.0));
+    colors.push_back(VEC3(1.0, 1.0, 0.0));
 
-  // bottom face
-  indices.push_back(VEC3I(4, 5, 0));
-  indices.push_back(VEC3I(0, 5, 1));
-  colors.push_back(VEC3(1.0, 0.0, 1.0));
-  colors.push_back(VEC3(1.0, 0.0, 1.0));
+    // bottom face
+    indices.push_back(VEC3I(4, 5, 0));
+    indices.push_back(VEC3I(0, 5, 1));
+    colors.push_back(VEC3(1.0, 0.0, 1.0));
+    colors.push_back(VEC3(1.0, 0.0, 1.0));
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 // build out a cube
 //////////////////////////////////////////////////////////////////////////////////
-void buildCubePerVertexColors(vector<VEC3>& vertices, vector<VEC3I>& indices, vector<VEC3>& colors)
-{
-  vertices.push_back(VEC3(-0.5, -0.5,  0.5));
-  vertices.push_back(VEC3( 0.5, -0.5,  0.5));
-  vertices.push_back(VEC3(-0.5,  0.5,  0.5));
-  vertices.push_back(VEC3( 0.5,  0.5,  0.5));
-  vertices.push_back(VEC3(-0.5, -0.5, -0.5));
-  vertices.push_back(VEC3( 0.5, -0.5, -0.5));
-  vertices.push_back(VEC3(-0.5,  0.5, -0.5));
-  vertices.push_back(VEC3( 0.5,  0.5, -0.5));
-  colors.push_back(VEC3(1.0, 0.0, 0.0));
-  colors.push_back(VEC3(1.0, 0.0, 0.0));
-  colors.push_back(VEC3(0.0, 1.0, 0.0));
-  colors.push_back(VEC3(0.0, 1.0, 0.0));
-  colors.push_back(VEC3(0.0, 0.0, 1.0));
-  colors.push_back(VEC3(0.0, 0.0, 1.0));
-  colors.push_back(VEC3(1.0, 1.0, 0.0));
-  colors.push_back(VEC3(1.0, 1.0, 0.0));
+void buildCubePerVertexColors(vector<VEC3>& vertices, vector<VEC3I>& indices,
+                              vector<VEC3>& colors) {
+    vertices.push_back(VEC3(-0.5, -0.5, 0.5));
+    vertices.push_back(VEC3(0.5, -0.5, 0.5));
+    vertices.push_back(VEC3(-0.5, 0.5, 0.5));
+    vertices.push_back(VEC3(0.5, 0.5, 0.5));
+    vertices.push_back(VEC3(-0.5, -0.5, -0.5));
+    vertices.push_back(VEC3(0.5, -0.5, -0.5));
+    vertices.push_back(VEC3(-0.5, 0.5, -0.5));
+    vertices.push_back(VEC3(0.5, 0.5, -0.5));
+    colors.push_back(VEC3(1.0, 0.0, 0.0));
+    colors.push_back(VEC3(1.0, 0.0, 0.0));
+    colors.push_back(VEC3(0.0, 1.0, 0.0));
+    colors.push_back(VEC3(0.0, 1.0, 0.0));
+    colors.push_back(VEC3(0.0, 0.0, 1.0));
+    colors.push_back(VEC3(0.0, 0.0, 1.0));
+    colors.push_back(VEC3(1.0, 1.0, 0.0));
+    colors.push_back(VEC3(1.0, 1.0, 0.0));
 
-  // front face
-  indices.push_back(VEC3I(0, 1, 2));
-  indices.push_back(VEC3I(2, 1, 3));
+    // front face
+    indices.push_back(VEC3I(0, 1, 2));
+    indices.push_back(VEC3I(2, 1, 3));
 
-  // back face
-  indices.push_back(VEC3I(5, 4, 7));
-  indices.push_back(VEC3I(7, 4, 6));
+    // back face
+    indices.push_back(VEC3I(5, 4, 7));
+    indices.push_back(VEC3I(7, 4, 6));
 
-  // left face
-  indices.push_back(VEC3I(4, 0, 6));
-  indices.push_back(VEC3I(6, 0, 2));
+    // left face
+    indices.push_back(VEC3I(4, 0, 6));
+    indices.push_back(VEC3I(6, 0, 2));
 
-  // right face
-  indices.push_back(VEC3I(1, 5, 3));
-  indices.push_back(VEC3I(3, 5, 7));
+    // right face
+    indices.push_back(VEC3I(1, 5, 3));
+    indices.push_back(VEC3I(3, 5, 7));
 
-  // top face
-  indices.push_back(VEC3I(2, 3, 6));
-  indices.push_back(VEC3I(6, 3, 7));
+    // top face
+    indices.push_back(VEC3I(2, 3, 6));
+    indices.push_back(VEC3I(6, 3, 7));
 
-  // bottom face
-  indices.push_back(VEC3I(4, 5, 0));
-  indices.push_back(VEC3I(0, 5, 1));
+    // bottom face
+    indices.push_back(VEC3I(4, 5, 0));
+    indices.push_back(VEC3I(0, 5, 1));
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
-int main(int argc, char** argv)
-{
-  return 0;
-}
+int main(int argc, char** argv) { return 0; }
