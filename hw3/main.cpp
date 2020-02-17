003c39c89a2fe10fc4959bcc146b30b5ff2aa17f
Sarim Abbas <sarim.stays@gmail.com>
2020-02-17 15:59:52 -0500

    finished viewport matrix

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -8,6 +8,18 @@
 
 using namespace std;
 
+// prototypes
+template <typename T, typename A>
+void printVector(vector<T, A> const& vec);
+
+vector<VEC3> viewportMatrix(vector<VEC3> vertices, int xRes, int yRes);
+void testViewportMatrix(vector<VEC3> vertices);
+
+int indexIntoPPM(int x, int y, int xRes, int yRes,
+                 bool originBottomLeft = false);
+float* allocatePPM(int xRes, int yRes);
+void initPPM(float* values, int xRes, int yRes);
+
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
 VEC3 truncate(const VEC4& v) { return VEC3(v[0], v[1], v[2]); }
@@ -95,16 +107,17 @@ void buildBigSquare(vector<VEC3>& vertices, vector<VEC3I>& indices,
 //////////////////////////////////////////////////////////////////////////////////
 void buildSquare(vector<VEC3>& vertices, vector<VEC3I>& indices,
                  vector<VEC3>& colors) {
+    //  the four points of the square
     vertices.push_back(VEC3(-0.5, -0.5, 0.5));
     vertices.push_back(VEC3(0.5, -0.5, 0.5));
     vertices.push_back(VEC3(-0.5, 0.5, 0.5));
     vertices.push_back(VEC3(0.5, 0.5, 0.5));
 
     // front face
-    indices.push_back(VEC3I(0, 1, 2));
-    indices.push_back(VEC3I(2, 1, 3));
-    colors.push_back(VEC3(1.0, 0.0, 0.0));
-    colors.push_back(VEC3(0.0, 1.0, 0.0));
+    indices.push_back(VEC3I(0, 1, 2));      // the first triangle
+    indices.push_back(VEC3I(2, 1, 3));      // the second triangle
+    colors.push_back(VEC3(1.0, 0.0, 0.0));  // the first triangle color
+    colors.push_back(VEC3(0.0, 1.0, 0.0));  // the second triangle color
 }
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -207,4 +220,115 @@ void buildCubePerVertexColors(vector<VEC3>& vertices, vector<VEC3I>& indices,
 
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
-int main(int argc, char** argv) { return 0; }
+int main(int argc, char** argv) {
+    // void buildSquare(vector<VEC3> & vertices, vector<VEC3I> & indices,
+    //                  vector<VEC3> & colors)
+
+    cout << "Starting build square" << endl;
+    vector<VEC3> vertices;
+    vector<VEC3I> indices;
+    vector<VEC3> colors;
+    // the pre-provided geometries are in the canonical view volume
+    // the canonical view volumne is a cube centered at origin, with edges
+    // that go from -1 to +1.
+    buildSquare(vertices, indices, colors);
+    // printVector(vertices);
+
+    // we do a viewport matrix transform, modifying each coordinate
+    vector<VEC3> transformedVertices = viewportMatrix(vertices, 800, 600);
+    testViewportMatrix(transformedVertices);
+
+    return 0;
+}
+
+vector<VEC3> viewportMatrix(vector<VEC3> vertices, int xRes, int yRes) {
+    // the viewport transform is basically a scaling operation
+    // it takes the canonical volume and stretches it to xRes * yRes * 2 box
+    // (the z-axis is unchanged)
+
+    MATRIX4 mvp;
+    mvp.setZero();
+
+    mvp(0, 0) = xRes / 2;
+    mvp(0, 3) = xRes / 2;
+    mvp(1, 1) = yRes / 2;
+    mvp(1, 3) = yRes / 2;
+    mvp(2, 2) = 1;
+    mvp(3, 3) = 1;
+
+    vector<VEC3> transformedVertices;
+    for (int i = 0; i < vertices.size(); i++) {
+        // first, convert the (3,1) vector to a (4,1)
+        VEC4 extended = extend(vertices[i]);
+        // multiply with the mvp
+        VEC4 multiplied = mvp * extended;
+        // truncate back to (3,1) vector
+        VEC3 truncated = truncate(multiplied);
+        // set that as the new vector
+        transformedVertices.push_back(truncated);
+    }
+
+    return transformedVertices;
+}
+
+// debugging routines
+
+template <typename T, typename A>
+void printVector(vector<T, A> const& vec) {
+    cout << "[";
+    for (auto i = vec.cbegin(); i != vec.cend(); ++i) {
+        cout << "{" << *i << "}" << endl;
+    };
+    cout << "]" << endl;
+}
+
+void testViewportMatrix(vector<VEC3> vertices) {
+    // write out the vertices to a file
+    int xRes = 800;
+    int yRes = 600;
+    float* ppm = allocatePPM(xRes, yRes);
+    for (int i = 0; i < vertices.size(); i++) {
+        int index = indexIntoPPM(vertices[i][0], vertices[i][1], xRes, yRes);
+        ppm[index] = 255;
+        ppm[index + 1] = 255;
+        ppm[index + 2] = 255;
+    }
+    writePPM("testViewportMatrix.ppm", xRes, yRes, ppm);
+    delete[] ppm;
+}
+
+// PPM routines
+
+int indexIntoPPM(int x, int y, int xRes, int yRes, bool originBottomLeft) {
+    int index = -1;
+
+    if (originBottomLeft) {
+        // bottom left origin
+        int newY = yRes - 1 - y;
+        index = (newY * xRes * 3) + (x * 3);
+
+    } else {
+        // top left origin (default)
+        index = (y * xRes * 3) + (x * 3);
+    }
+
+    return index;
+}
+
+float* allocatePPM(int xRes, int yRes) {
+    float* values = new float[3 * xRes * yRes];
+    if (!values) {
+        cout << "Could not allocate values" << endl;
+        return NULL;
+    }
+    initPPM(values, xRes, yRes);
+    return values;
+}
+
+void initPPM(float* values, int xRes, int yRes) {
+    for (int i = 0; i < xRes * yRes * 3; i += 3) {
+        values[i] = 0;
+        values[i + 1] = 0;
+        values[i + 2] = 0;
+    }
+}
